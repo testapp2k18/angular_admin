7b335f3a772be8ebdb31a1ac4fc72b6d10086896
Pritam Ghosh <pritamghosh.brainium@gmail.com>
2019-02-27 18:25:32 +0530

    delete all admin files

--- src/app/dashboard/myprofile/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This barrel file provides the export for the lazy loaded FormsComponent.
- */
-export * from './myprofile.component';
-export * from './myprofile.routes';
--- src/app/pipes/pipes.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { UrlPipe } from './url/url';
-
-@NgModule({
-	declarations: [UrlPipe],
-	imports: [],
-	exports: [UrlPipe]
-})
-export class PipesModule {}
